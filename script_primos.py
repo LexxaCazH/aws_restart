964dbec4d1b0d4176752312f3997d6bb0bd5f98c
LexxaCazH <156867401+LexxaCazH@users.noreply.github.com>
2024-01-22 20:02:25 -0600

    Update script_primos.py
    Muestra todos los números primos entre 1 y 250.
    Almacene los resultados en un archivo results.txt .

--- script_primos.py
+++ script_primos.py
@@ -1,4 +1,4 @@
-def es_primo(num):
+def primo(num):
     if num < 2:
         return False
     for i in range(2, int(num**0.5) + 1):
@@ -6,11 +6,9 @@ def es_primo(num):
             return False
     return True
 
-# Encuentra y almacena los números primos entre 1 y 250
-primos = [str(num) for num in range(1, 251) if es_primo(num)]
+primos = [str(num) for num in range(1, 251) if primo(num)]
 
-# Almacena los resultados en un archivo
 with open("results.txt", "w") as archivo:
     archivo.write("\n".join(primos))
 
-print("Números primos entre 1 y 250 almacenados en results.txt.")
+print("Números primos entre 1 y 250")
